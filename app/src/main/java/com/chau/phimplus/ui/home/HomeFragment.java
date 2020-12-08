13d0454bf6102c6ffe5605dbccb2c3a736823fad
Nguyen Chau <soc900k@gmail.com>
2020-12-08 07:39:47 +0700

    Đã chạy trơn tru
    Code hậu bị thiếu thư viện GridLayoutManager (đã thêm)

--- app/src/main/java/com/chau/phimplus/ui/home/HomeFragment.java
+++ app/src/main/java/com/chau/phimplus/ui/home/HomeFragment.java
@@ -16,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
